ad5bf814575be727c1bc7b673842c55cbe190308
JobGetabu <getabujob@gmail.com>
2019-10-21 11:19:58 +0300

    Initial Commit

---
+++ NavigationTut/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NavigationTut
+//
+//  Created by project x on 21/10/2019.
+//  Copyright © 2019 project x. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
